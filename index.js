bbe1aedce840b5c7838d471dcde28ec3fee5c00a
vigneshvicky3122@gmail.com <vigenshvicky3122@gamil.com>
2023-01-18 15:20:21 +0530

    final

--- index.js
+++ index.js
@@ -54,8 +54,7 @@ app.post("/signup", async function (req, res) {
           email: req.body.email,
           username: req.body.username,
           password: hashedPassword,
-          loginUrl:
-            "https://url-shortener-web-apps.netlify.app/instagram/user/accounts/my_accounts/email/login",
+          loginUrl: "/instagram/user/accounts/my_accounts/email/login",
           shortUrl: shortUrl,
           Active: false,
           clickCount: 0,
